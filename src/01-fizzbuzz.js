9cec4f01b735684b67dd3084e49a6dd2c11b7db7
Cece Claridge <cece.claridge@gmail.com>
2020-12-10 13:17:07 -0500

    add fizzbuzz function

--- src/01-fizzbuzz.js
+++ src/01-fizzbuzz.js
@@ -8,7 +8,14 @@ For a given natural number greater than zero return:
 */
 
 function fizzbuzz(number) {
-  return true;
+    for (let i = 1; i <= n; i++) {
+    let str = "";
+
+    if (i % 3 === 0) str += "fizz"
+    if (i % 5 === 0) str += "buzz"
+    if (str === "") str = i;
+  
+    console.log(str);
 }
 
 module.exports = fizzbuzz;
