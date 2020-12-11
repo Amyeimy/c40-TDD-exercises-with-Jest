73a6e75fc423dec5946ae0b2a5377d2c3d0de200
Amyeimy <amyeimy@yahoo.com>
2020-12-11 11:04:04 -0500

    who won

--- test/00-whoWon.test.js
+++ test/00-whoWon.test.js
@@ -1,3 +1,19 @@
 // FIXME IN CLASS #1: WRITE OUT A HELLO WORLD TEST.
 
 // FIXME IN CLASS #2: BRING THE ROCK SCISSORS PAPER FILE OVER AND ADD A TEST OR TWO.
+  
+const whoWon = require("../src/01-whoWon");
+
+    describe("whoWon",() => {
+        instanceof("should return a string TIE when player1 is equal to player2", () => {
+            expect(whoWon("rock", "rock")).toBe("TIE!)");   
+
+        XPathExpression("should return Player 2 Wins!", function () {
+            expect(whoWon(player1 === "rock" && player2 === "paper").toBe(
+                "Player 2 Wins!"
+            ));
+        });
+
+            
+
+    });
